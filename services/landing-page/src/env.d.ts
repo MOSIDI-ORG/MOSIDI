727bd1c21b763dacb2bc75ffb4ed38827cec5471
Sebastian Meier <contact@sebastianmeier.eu>
2025-10-10 23:18:41 +0200

    adding landing page

---
+++ services/landing-page/src/env.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="../../frontend/public/landing-page/.astro/types.d.ts" />
+/// <reference path="../../frontend/dist/landing-page/.astro/types.d.ts" />
+/// <reference path="../.astro/types.d.ts" />
