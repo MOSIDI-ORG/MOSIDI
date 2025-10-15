80c4e018d3456baacfa9c08c9fd6d78cd6513bcd
qsafariallahkheili <safari.qassem@gmail.com>
2025-10-15 11:51:01 +0200

    filter options for custom formula

--- services/frontend/src/components/CustomIndicatorUI.vue
+++ services/frontend/src/components/CustomIndicatorUI.vue
@@ -1,10 +1,110 @@
 <template>
     <div style="overflow-x: hidden;" >
+        <v-card density="compact" width="371" style="background-color: black; color: white;">
+            <div style="padding: 8px;">
+                <v-text-field
+                    :label="$t('dataset-filter.search')"
+                    prepend-inner-icon="mdi-magnify"
+                    class="expanding-search"
+                    filled
+                    outlined
+                    density="compact"
+                    clearable
+                    dense
+                    single-line
+                    hide-details
+                    v-model="layerSearchText"
+                >
+                </v-text-field>
+                            
+            </div>
+            <div class="mb-4 ml-2 mr-2"  >
+                <v-row no-gutters>
+                    <v-col >
+                        <v-select
+                            :items="datasetTypes"
+                            :item-title="'alias'"
+                            :item-value="'name'"
+                            :label="$t('dataset-filter.filter-label.type')"
+                            dense
+                            outlined
+                            density="compact"
+                            single-line
+                            hide-details
+                            rounded
+                            solo                
+                            v-model="selectedDatasetType"
+                        ></v-select>
+                    </v-col>
+                    <v-col>
+                        <v-select
+                            :items=dataSources
+                            item-value="value"
+                            item-title="label"
+                            :label="$t('dataset-filter.filter-label.source')"
+                            dense
+                            outlined
+                            single-line
+                            density="compact"
+                            hide-details
+                            rounded
+                            solo 
+                            v-model="selectedDatasetSource"
+                        >
+                            
+                        </v-select>
+                    </v-col>
+                    
+                    
+                </v-row>
+                <v-row no-gutters>
+                    <v-col>
+                        <v-select
+                            :items=geometryTypes
+                            item-value="value"
+                            item-title="label"
+                            :label="$t('dataset-filter.filter-label.geometry')"
+                            dense
+                            outlined
+                            single-line
+                            hide-details
+                            rounded
+                                density="compact"
+                            solo 
+                            v-model="selectedGeometryTypee"
+                        >
+                        
+                    </v-select>
+                    </v-col>
+                    <v-col>
+                        <v-select
+                            :items=availableYearsForIndicatorFilter
+                            item-value="value"
+                            item-title="label"
+                            :label="$t('dataset-filter.filter-label.time')"
+                            dense
+                            outlined
+                            single-line
+                            hide-details
+                            rounded
+                                density="compact"
+                            solo 
+                            v-model="selectedYearIndicatorFilter"
+                        >
+                            
+                        </v-select>
+                    </v-col>
+                </v-row>
+            </div>
+
+        </v-card>
+       
+
         <div style="background-color:transparent; overflow-y: scroll; max-height: 300px;">
-            
-            <v-list lines="two" style="background-color:transparent; height: 81%;" class="ml-1 mr-1">           
+            <v-list lines="two" style="background-color:transparent; height: 81%;" class="ml-1 mr-1">   
+                <span style="font-size: 1rem; font-weight: 500;" class="ml-2">{{filteredItems?.length + ' '+  $t('dataset-filter.results')}}</span>        
                 <v-list-item
-                    v-for="(metadata, index) in indicatorNames"
+                    v-for="(metadata, index) in filteredItems"
                     :key="index"
                     style="border-radius: 5px;"
                     @click=handleItemClick(metadata)
@@ -98,14 +198,22 @@
 import { defineProps, ref, computed, onMounted, defineEmits } from 'vue'
 import {getIndicatorData, getGeojsonDataFromDB, classification} from "../services/backend.calls";
 import { useAlertStore } from '@/stores/alert'
+import { useDatasetSearchStore } from '../stores/datasetSearch'
+import { storeToRefs } from 'pinia'
 
 const alertStore = useAlertStore()
+let {  activatedDatasetSearch } = storeToRefs(useDatasetSearchStore())
+
 const emit = defineEmits(["addDeckglLayer", "updateDeckglLayer", "customMapStylization", "addCustomLayer"]);
 
 let kommunales_gebiet_geojson = ref(null)
 let classification_result = ref(null)
 let hoveredItem = ref(null)
-
+let layerSearchText= ref("")
+let selectedGeometryTypee = ref(null)
+let selectedDatasetSource = ref(null)
+let selectedYearIndicatorFilter = ref(null)
+let selectedDatasetType = ref(null)
 
 onMounted(()=>{
    
@@ -113,7 +221,107 @@ onMounted(()=>{
 
 })
     
+const filteredItems = computed(() => {
+    return props.indicatorNames?.filter(item => {
+        const matchesSearchText = layerSearchText.value
+            ? item.dct_title.toLowerCase().includes(layerSearchText.value.toLowerCase())
+            : true;
+        const matchesDatasetType = selectedDatasetType.value && selectedDatasetType.value !== 'all'
+            ? item.dct_type === selectedDatasetType.value
+            : true;
+        const matchesDatasetSource = selectedDatasetSource.value && selectedDatasetSource.value !== 'All'
+            ? item.dct_catalog_publisher === selectedDatasetSource.value
+            : true;
+        const matchesGeometryType = selectedGeometryTypee.value && selectedGeometryTypee.value !== 'All'
+            ? item.geometry_type === selectedGeometryTypee.value
+            : true;
 
+        const matchesDatasetYear = selectedYearIndicatorFilter.value && selectedYearIndicatorFilter.value !== 'All'
+            ? new Date(item.dct_temporal_enddate).getFullYear() >= parseInt(selectedYearIndicatorFilter.value)
+            : true;
+       
+        
+        return matchesSearchText && matchesDatasetType && matchesDatasetSource && matchesGeometryType && matchesDatasetYear;
+    });
+});
+const datasetTypes = computed(() => {
+  if (activatedDatasetSearch.value === 'indicator') {
+    return [{ alias: 'Indicator', name: 'indikator' }];
+  } else if (activatedDatasetSearch.value === 'geodata') {
+    return [{ alias: 'WMS', name: 'raster' }];
+  } else {
+    return [
+      { alias: 'Indicator', name: 'indikator' },
+      { alias: 'WMS', name: 'raster' },
+      { alias: 'All', name: 'all' },
+    ];
+  }
+});
+const filteredMeta = computed(() => {
+  if (!props.indicatorNames) return []
+
+  const typeFilter = activatedDatasetSearch.value?.toLowerCase()
+
+  return props.indicatorNames?.filter(item => {
+    if (!item.dct_type) return false
+    const itemType = item.dct_type.trim().toLowerCase()
+
+    if (typeFilter === 'indicator') return itemType === 'indikator'
+    if (typeFilter === 'geodata') return itemType === 'raster'
+    return true
+  })
+})
+// reactive dataSources with counts
+const dataSources = computed(() => {
+  const counts = filteredMeta.value.reduce((acc, item) => {
+    const key = item.dct_catalog_publisher
+    acc[key] = (acc[key] || 0) + 1
+    return acc
+  }, {})
+
+  return [
+    ...Object.entries(counts).map(([value, count]) => ({
+      value,
+      count,
+      label: `${value} (${count})`
+    }))
+    .sort((a, b) =>
+      a.value.localeCompare(b.value, 'de', { sensitivity: 'base' }) // handles German chars
+    ),
+    { value: 'All', count: filteredMeta.value.length, label: `All (${filteredMeta.value.length})` }
+  ]
+})
+// reactive geometryTypes with counts
+const geometryTypes = computed(() => {
+  const counts = filteredMeta.value.reduce((acc, item) => {
+    const key = item.geometry_type
+    acc[key] = (acc[key] || 0) + 1
+    return acc
+  }, {})
+
+  return [
+    ...Object.entries(counts).map(([value, count]) => ({
+      value,
+      count,
+      label: `${value} (${count})`
+    })),
+    { value: 'All', count: filteredMeta.value.length, label: `All (${filteredMeta.value.length})` }
+  ]
+})
+
+// reactive availableYears with counts
+const availableYearsForIndicatorFilter = computed(() => {
+  const years = filteredMeta.value.map(item => new Date(item.dct_temporal_enddate).getFullYear())
+  const uniqueYears = Array.from(new Set(years)).sort((a,b)=>a-b)
+
+  return [
+    ...uniqueYears.map(year => {
+      const count = years.filter(y => y >= year).length
+      return { value: year, count, label: `${year} (${count})` }
+    }),
+    { value: 'All', count: filteredMeta.value.length, label: `All (${filteredMeta.value.length})` }
+  ]
+})
 
 const getKommunalesGebietCentroidGeojson = async () => {
     if(kommunales_gebiet_geojson.value == null ){
