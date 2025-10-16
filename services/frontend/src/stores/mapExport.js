c1331c0cb45554a0bf7baf480c4ebb2a661d7777
qsafariallahkheili <safari.qassem@gmail.com>
2025-10-16 11:27:41 +0200

    export items

--- services/frontend/src/stores/mapExport.js
+++ services/frontend/src/stores/mapExport.js
@@ -4,7 +4,10 @@ export const useMapExportStore = defineStore ({
     id: 'mapExport',
     state: () => ({
         exportDialog: false,
-        formats: ['png', 'jpeg', 'pdf'],
+        formats: ['JPEG', 'PNG', 'SVG', 'PDF'],
+        pageSize:["LETTER", "A2", "A3", "A4", "A5", "A6", "B2", "B3", "B4", "B5", "B6" ],
+        DPI: [72, 96, 200, 300, 400],
+        pageOrientation: ['Landscape', 'Portrait'],
         report: ''
     })
 })
