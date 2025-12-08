82cb2b13c4b23bf37eebd60d1936674828b76c31
Sebastian Meier <contact@sebastianmeier.eu>
2025-12-08 13:37:47 +0100

    fix sutbtitles

--- services/landing-page/src/env.d.ts
+++ services/landing-page/src/env.d.ts
@@ -1,3 +1,7 @@
 /// <reference path="../../frontend/public/landing-page/.astro/types.d.ts" />
 /// <reference path="../../frontend/dist/landing-page/.astro/types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
+declare module '*.vtt?url' {
+  const src: string;
+  export default src;
+}
